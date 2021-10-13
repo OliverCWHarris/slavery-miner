d306a906f5c5e3301e967cf7652b83244846b06e
Oliver Harris <olivercwharris@gmail.com>
2021-10-13 13:32:50 +0100

    added menu, added crate command

--- stolen_code.py
+++ stolen_code.py
@@ -22,7 +22,7 @@ def opendiscord():
     pyautogui.click()#enters chat
     time.sleep(0.1)
 
-def writecommands():
+def allcommands():
     #typing commands
     pyautogui.write(';f')#fish command
     pyautogui.press('enter')
@@ -40,6 +40,37 @@ def writecommands():
     pyautogui.press('enter')
     time.sleep(randomtime)
 
+def justsell():
+    pyautogui.write(';f')#fish command
+    pyautogui.press('enter')
+    time.sleep(randomtime)
+    pyautogui.write(';h')#hunt command
+    pyautogui.press('enter')
+    time.sleep(randomtime)
+    pyautogui.write(';s')#sell command
+    pyautogui.press('enter')
+
+def crates():
+    pyautogui.write(';f')#fish command
+    pyautogui.press('enter')
+    time.sleep(randomtime)
+    pyautogui.write(';h')#hunt command
+    pyautogui.press('enter')
+    time.sleep(randomtime)
+    pyautogui.write(';s')#sell command
+    pyautogui.press('enter')
+    pyautogui.write(';hourly')
+    pyautogui.press('enter')
+    time.sleep(randomtime)
+    pyautogui.write(';daily')
+    pyautogui.press('enter')
+    time.sleep(randomtime)
+    pyautogui.write(';weekly')
+    pyautogui.press('enter')
+
+
+
+
 def closediscord():
     #mimnimises discord
     pyautogui.moveTo(320,1050)#goes to discord in the tray
@@ -52,13 +83,28 @@ def closediscord():
 start=1
 repeated=0
 while start==1:
-    opendiscord()
-    time.sleep(2)
-    writecommands()
-    time.sleep(2)
-    closediscord()
+    menuchoice=int(input("1 for everything\n2 for sell\n3 for crates\n:"))
+    if menuchoice==1:
+        opendiscord()
+        time.sleep(2)
+        allcommands()
+        time.sleep(2)
+        closediscord()
+    elif menuchoice==2:
+        opendiscord()
+        time.sleep(2)
+        justsell()
+        time.sleep(2)
+        closediscord()
+    elif menuchoice==3:
+        opendiscord()
+        time.sleep(2)
+        crates()
+        time.sleep(2)
+        closediscord()
+    
+    
     #time.sleep(420)
     #repeated=repeated+1
     #if repeated==10:
     #   start=0
-    start=0
